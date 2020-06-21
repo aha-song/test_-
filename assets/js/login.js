0a9e109fabce94f0df0d2584cb9ac42500d46f35
song <1195061008@qq.com>
2020-06-21 09:47:12 +0800

    完成登录页面的验证

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,9 +5,16 @@ $(function(){
     $('.reg-box').show()
   })
 
-  // 点击“去登录”的链接
+  // 点击去登录的链接
   $('#link_login').on('click', function() {
     $('.login-box').show()
     $('.reg-box').hide()
+  })
+
+  var form = layui.form
+  form.verify({
+    pwd: [
+      /^[\S]{6,12}$/,'密码必须6到12位，且不能出现空格'
+    ] 
   })
 })
